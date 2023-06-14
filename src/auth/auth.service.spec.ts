c6dd46a7ce7beafb9a7357c1ccc70e4e1546de78
cheesechoux <gsoosuu@gmail.com>
2023-06-14 16:47:56 +0900

    feat: test-app.module.ts

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,74 +3,24 @@ import { AuthService } from '@src/auth/auth.service';
 import { User } from '@src/users/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDTO } from '@src/auth/dto/auth.dto';
-import { AuthModule } from '@src/auth/auth.module';
-import { JwtStrategy } from '@src/auth/jwt/jwt.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeOrm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import { UsersModule } from '@src/users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { TestAppModule } from '@src/test-app.module';
 
 describe('AuthService', () => {
     let authService: AuthService;
     let usersRepository: Repository<User>;
 
-    const bcryptMock = {
-        genSalt: jest.fn(() => Promise.resolve('salt')),
-        hashSync: jest.fn((password, salt) => bcrypt.hashSync(password, salt)),
-    };
-
     beforeEach(async () => {
         const module = await Test.createTestingModule({
-            imports: [
-                ConfigModule.forRoot({
-                    isGlobal: true,
-                }),
-                TypeOrmModule.forRootAsync({
-                    imports: [
-                        ConfigModule,
-                        PassportModule,
-                        JwtModule.registerAsync({
-                            imports: [ConfigModule],
-                            inject: [ConfigService],
-                            useFactory: async (config: ConfigService) => ({
-                                secret: config.get('JWT_SECRET'),
-                                signOptions: {
-                                    expiresIn: config.get('JWT_EXP'),
-                                },
-                            }),
-                        }),
-                    ],
-                    inject: [ConfigService],
-                    useFactory: (configService: ConfigService) => ({
-                        type: 'mysql',
-                        host: configService.get('DB_HOST'),
-                        port: 3306,
-                        username: configService.get('DB_USERNAME'),
-                        password: configService.get('DB_PASSWORD'),
-                        database: configService.get('DB_NAME'),
-                        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-                        synchronize: false,
-                        autoLoadEntities: true,
-                        logging: false,
-                        socketPath: '/tmp/mysql.sock',
-                        namingStrategy: new SnakeNamingStrategy(),
-                    }),
-                }),
-                TypeOrmModule.forFeature([User]),
-                AuthModule,
-                UsersModule,
-            ],
+            imports: [TestAppModule],
             providers: [
-                AuthService,
-                JwtStrategy,
-                JwtService,
                 {
                     provide: 'bcrypt',
-                    useValue: bcryptMock,
+                    useValue: {
+                        genSalt: jest.fn(() => Promise.resolve('salt')),
+                        hashSync: jest.fn((password, salt) => bcrypt.hashSync(password, salt)),
+                    },
                 },
             ],
         }).compile();
@@ -106,35 +56,6 @@ describe('AuthService', () => {
             expect(hashSpy).toHaveBeenCalledWith(createUserDTO.password, expect.any(String));
             expect(saveSpy).toHaveBeenCalledWith(expect.objectContaining(createUserDTO));
             expect(result).toEqual(createUserDTO);
-        });
-
-        it('should create a new user', async () => {
-            // Given
-            const createUserDTO = {
-                name: 'jisoo',
-                email: 'jisoo@test.com',
-                password: 'hashedPassword',
-            };
-
-            const findOneSpy = jest.spyOn(usersRepository, 'findOne');
-            findOneSpy.mockResolvedValue(null);
-
-            const saveSpy = jest.spyOn(usersRepository, 'save');
-            const savedUser = new User();
-            Object.assign(savedUser, createUserDTO);
-            saveSpy.mockResolvedValue(savedUser);
-
-            const hashSpy = jest.spyOn(bcrypt, 'hash');
-            hashSpy.mockImplementation(async () => 'hashedPassword');
-
-            // When
-            const result = await authService.signUp(createUserDTO);
-
-            // Then
-            expect(findOneSpy).toHaveBeenCalledWith({ where: { email: createUserDTO.email } });
-            expect(hashSpy).toHaveBeenCalledWith(createUserDTO.password, expect.any(String));
-            expect(saveSpy).toHaveBeenCalledWith(expect.any(User));
-            expect(result).toEqual(savedUser);
         });
     });
 });
---
+++ src/test-app.module.ts
@@ -0,0 +1,69 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { AuthService } from '@src/auth/auth.service';
+import { JwtStrategy } from '@src/auth/jwt/jwt.strategy';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '@src/users/user.entity';
+import { AuthModule } from '@src/auth/auth.module';
+import { UsersModule } from '@src/users/users.module';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [
+                ConfigModule,
+                PassportModule,
+                JwtModule.registerAsync({
+                    imports: [ConfigModule],
+                    inject: [ConfigService],
+                    useFactory: async (config: ConfigService) => ({
+                        secret: config.get('JWT_SECRET'),
+                        signOptions: {
+                            expiresIn: config.get('JWT_EXP'),
+                        },
+                    }),
+                }),
+            ],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('DB_HOST'),
+                port: 3306,
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_NAME'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: false,
+                autoLoadEntities: true,
+                logging: false,
+                socketPath: '/tmp/mysql.sock',
+                namingStrategy: new SnakeNamingStrategy(),
+            }),
+        }),
+        TypeOrmModule.forFeature([User]),
+        AuthModule,
+        UsersModule,
+    ],
+    providers: [
+        AuthService,
+        JwtStrategy,
+        JwtService,
+        {
+            provide: 'bcrypt',
+            useValue: {
+                genSalt: jest.fn(() => Promise.resolve('salt')),
+                hashSync: jest.fn((password, salt) => bcrypt.hashSync(password, salt)),
+            },
+        },
+    ],
+})
+export class TestAppModule {}
